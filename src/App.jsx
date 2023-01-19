cf433a32b11e713db6502be12dab029c1e2cd381
Tyler Davis <tallyndavis@comcast.net>
2023-01-19 15:24:38 -0500

    first app commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Main from './components/Main.jsx'
+import Navbar from './components/Navbar.jsx'
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
